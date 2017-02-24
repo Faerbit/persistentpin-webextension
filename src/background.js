11709adae3af12b71f87c05a9f2ddcbf8651f8c3
Faerbit <faerbit@gmail.com>
2017-02-24 02:39:34 +0100

    Actually open the pinned tabs.

--- src/background.js
+++ src/background.js
@@ -1,4 +1,22 @@
+// only open on the inital startup
+var opened_tabs = false;
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function openTabs(item) {
+    var pinned_websites = item.pinned_websites;
+    pinned_websites.forEach(function(website) {
+        browser.tabs.create({url: website, "pinned": true,
+            "active": false});
+    });
+    opened_tabs = true;
+}
+
 browser.windows.onCreated.addListener(function() {
-    browser.tabs.create({url: "https://github.com", "pinned": true,
-        "active": false});
+    if (opened_tabs == false) {
+        let getWebsites = browser.storage.local.get("pinned_websites");
+        getWebsites.then(openTabs, onError);
+    }
 });
