5eb3f088d46a97d97b3a6b5f2aad7d89f61aab6c
Faerbit <faerbit@gmail.com>
2017-02-24 01:18:59 +0100

    Actually storing the settings.

--- src/options.js
+++ src/options.js
@@ -1,5 +1,21 @@
-var pinned_websites = [ "https://facebook.com", "https://github.com", "https://twitter.com" ];
+// global variables
+
+var pinned_websites = null;
 var selection = null;
+var sync = null;
+
+function save() {
+    if (sync == true) {
+        let settingWebsites =
+            browser.storage.sync.set({"pinned_websites": pinned_websites});
+        settingWebsites.then(null, onError);
+    }
+    else {
+        let settingWebsites =
+            browser.storage.local.set({"pinned_websites": pinned_websites});
+        settingWebsites.then(null, onError);
+    }
+}
 
 function select(event) {
     Array.from(document.getElementsByClassName("selected"))
@@ -55,6 +71,7 @@ function up(event) {
         var tmp = pinned_websites[index - 1];
         pinned_websites[index - 1] = pinned_websites[index];
         pinned_websites[index] = tmp;
+        save();
         selection.id = parseInt(selection.id) - 1;
     }
     renderTable();
@@ -72,6 +89,7 @@ function down(event) {
         var tmp = pinned_websites[index + 1];
         pinned_websites[index + 1] = pinned_websites[index];
         pinned_websites[index] = tmp;
+        save();
         selection.id = parseInt(selection.id) + 1;
     }
     renderTable();
@@ -83,6 +101,7 @@ function _delete(event) {
     }
     var index = parseInt(selection.id);
     pinned_websites.splice(index, 1);
+    save();
     selection = null;
     renderTable();
 }
@@ -96,6 +115,7 @@ function grab(event) {
         tabs.forEach(function(element) {
             pinned_websites.push(element.url);
         });
+        save();
         selection = null;
         renderTable();
     });
@@ -116,10 +136,12 @@ function edit(event) {
             var index = parseInt(selection.id);
             if (editField.value != "") {
                 pinned_websites[index] = editField.value;
+                save();
                 renderTable(selection.id);
             }
             else {
                 pinned_websites.splice(index, 1);
+                save();
                 selection = null;
                 renderTable();
             }
@@ -164,11 +186,13 @@ function add(event) {
             if (editField.value != "") {
                 if (selection == null) {
                     pinned_websites.push(editField.value);
+                    save();
                     renderTable(pinned_websites.length - 1);
                 }
                 else {
                     pinned_websites.splice(parseInt(selection.id) + 1, 0,
                         editField.value);
+                    save();
                     renderTable(parseInt(selection.id) + 1);
                 }
             }
@@ -185,14 +209,55 @@ function add(event) {
     editField.focus();
 }
 
+function setSync(event) {
+    let val = document.getElementById("ckb-sync").checked;
+    let settingSync = browser.storage.local.set({"sync": val});
+    settingSync.then(null, onError);
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
 function init() {
-    renderTable();
+    let gettingSync = browser.storage.local.get("sync");
+    gettingSync.then(startLoading, onError);
+
+    document.getElementById("ckb-sync").addEventListener("click", setSync);
+
     document.getElementById("btn-grab").addEventListener("click", grab);
     document.getElementById("btn-add").addEventListener("click", add);
     document.getElementById("btn-edit").addEventListener("click", edit);
     document.getElementById("btn-up").addEventListener("click", up);
     document.getElementById("btn-down").addEventListener("click", down);
     document.getElementById("btn-delete").addEventListener("click", _delete);
+}
+
+function startLoading(item) {
+    sync = item.sync;
+    if (sync == null) {
+        sync = true;
+        setSync();
+    }
+    document.getElementById("ckb-sync").checked = sync;
+    if (sync == true) {
+        let gettingWebsites = browser.storage.sync.get("pinned_websites");
+        gettingWebsites.then(finishLoading, onError);
+    }
+    else {
+        let gettingWebsites = browser.storage.local.get("pinned_websites");
+        gettingWebsites.then(finishLoading, onError);
+    }
+}
+
+function finishLoading(item) {
+    if (item.pinned_websites == null) {
+        pinned_websites = []
+    }
+    else {
+        pinned_websites = item.pinned_websites
+    }
+    renderTable();
 }
 
 document.addEventListener("DOMContentLoaded", init());
